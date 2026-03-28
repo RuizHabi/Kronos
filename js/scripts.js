fc9dfd849a76f338eb29b9ecc5c6a46b829d5f71
Javier Ruiz <93483870+RuizHabi@users.noreply.github.com>
2026-03-28 12:12:22 +0900

    N2 wasn't showing

--- js/scripts.js
+++ js/scripts.js
@@ -57,7 +57,7 @@ function renderChart() {
           label: 'N2',
           passingScore: 90,
           scores: [
-              { date: new Date('2025-12-07'), totalScore: 49, details: { Vocabulary_Grammar: 13, Reading: 8, Listening: 28 }}
+              { date: new Date('2025-12-07'), totalScore: 49, details: { Vocabulary_Grammar_Reading: 13, Reading: 8, Listening: 28 }}
           ]
       },
       {
